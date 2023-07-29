16b3cd341066e56779bf7bad7a703913a3513a1e
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-07-29 17:36:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+let customerName = "bob";
+
+function upperCaseCustomerName() {
+  let customerName = "bob";
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+let leastFavoriteCustomer = "John Doe";
+
+function changeLeastFavoriteCustomer() {
+  let leastFavoriteCustomer = "Jane Smith";
+  return leastFavoriteCustomer;
+}
+
+console.log(upperCaseCustomerName());
+setBestCustomer();
+console.log(bestCustomer);
+overwriteBestCustomer();
+console.log(bestCustomer);
+console.log(changeLeastFavoriteCustomer());
+
+
+
